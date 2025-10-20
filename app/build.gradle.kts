a6ce036addb3fcfec4ed359159df9132a73d8c06
noelpatata <novocorboudnoel@gmail.com>
2025-10-20 17:07:17 +0200

    fixed production variables

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,9 @@ android {
         }
         release {
             buildConfigField("String", "API_BASE_URL", "\"https://api.wallettracker.downops.win\"")
+            buildConfigField("String", "API_VERSION", "\"1\"")
+            buildConfigField("String", "DEFAULT_USER", "\"\"")
+            buildConfigField("String", "DEFAULT_PASSWORD", "\"\"")
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
